41e65109a58ee4db7cf10cd6a97400f571b64329
Tim Hutton <tihutt@microsoft.com>
2020-10-20 21:01:10 +0100

    Added calculations from moon to earth, to test the maths. Avoiding infinite loop

--- code2.js
+++ code2.js
@@ -17,12 +17,22 @@
 
 
 function init() {
+    // debugging
     var earth_mass = 5.972e24; // kg
     var earth_radius = 6371e3; // m
+    var moon_distance = 384400e3; // m
     var d = pointMassFreeFallDistance(3.0, earth_mass, earth_radius);
     console.log("Distance fallen near Earth's surface in 3 seconds =",d,'m');
-    var t = pointMassFreeFallTime(earth_radius, earth_radius-d, earth_mass);
+    var t = pointMassFreeFallTime(earth_radius, earth_radius - d, earth_mass);
     console.log("Time taken to fall",d,"m near Earth's surface =",t,'s');
+    var d2 = pointMassFreeFallDistance(3.0, earth_mass, moon_distance);
+    console.log("Distance fallen towards Earth from Moon's orbit distance in 3 seconds =",d2,'m');
+    var t2 = pointMassFreeFallTime(moon_distance, moon_distance - d2, earth_mass);
+    console.log("Time taken to fall",d2,"m towards Earth from Moon's orbit distance =",t2,'s');
+    var t3 = pointMassFreeFallTime(moon_distance, earth_radius, earth_mass);
+    console.log("Time taken to fall",moon_distance-earth_radius,"m to Earth from Moon's orbit distance =",t3,'s, or',t3/(60.0*60.0*24.0),'days');
+    var d3 = pointMassFreeFallDistance(t3, earth_mass, moon_distance);
+    console.log("Distance fallen towards Earth from Moon's orbit distance in",t3,"seconds =",d3,'m');
 }
 
 function pointMassFreeFallDistance(time, planet_mass, initial_height) {
@@ -34,7 +44,7 @@ function pointMassFreeFallDistance(time, planet_mass, initial_height) {
     var x = Math.pow( (3.0/2.0) * ( (Math.PI/2.0) - time*Math.sqrt( (2.0*mu) / Math.pow(initial_height, 3.0) ) ), 2.0/3.0 );
     return initial_height * ( x - (1.0/5.0)*x*x - (3.0/175.0)*x*x*x - (23.0/7875.0)*x*x*x*x - (1894.0/3931875.0)*x*x*x*x*x ); // plus more terms?
     */
-    
+
     // so we solve numerically instead by bisection on the function pointMassFreeFallTime()
     var low_final_height_estimate = initial_height;
     var high_final_height_estimate = 0.0;
@@ -42,7 +52,8 @@ function pointMassFreeFallDistance(time, planet_mass, initial_height) {
     var t_low = pointMassFreeFallTime(initial_height, low_final_height_estimate, planet_mass);
     var t_mid = pointMassFreeFallTime(initial_height, mid_final_height_estimate, planet_mass);
     var t_high = pointMassFreeFallTime(initial_height, high_final_height_estimate, planet_mass);
-    while(t_high-t_low > 1e-10) {
+    var i = 0;
+    while(i++ < 100 && t_high-t_low > 1e-4) {
         if(time < t_mid) {
             high_final_height_estimate = mid_final_height_estimate;
             t_high = t_mid;
@@ -63,7 +74,7 @@ function pointMassFreeFallTime(initial_height, final_height, planet_mass) {
     var G = 6.674e-11; // universal gravitational constant
     var mu = G * planet_mass; // standard gravitational parameter
     return Math.sqrt( Math.pow(initial_height,3.0)/(2.0*mu) ) *
-        ( Math.sqrt((final_height/initial_height)*(1.0-(final_height/initial_height))) + 
+        ( Math.sqrt((final_height/initial_height)*(1.0-(final_height/initial_height))) +
           Math.acos(Math.sqrt(final_height/initial_height)) );
 }
 
